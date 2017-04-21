b551d7b6bef5e6ddc0ccd66955c60fa9f8916a1e
elseSL <els_lok@hotmail.com>
2017-04-21 15:28:29 +0200

    update db en niet af lid inschr.

--- app/codeproduction/controls/LidController.php
+++ app/codeproduction/controls/LidController.php
@@ -6,95 +6,85 @@ use \php\controls\AbstractController as AbstractController;
 
 class LidController extends AbstractController {
     public function __construct($control, $action){
-        parent::__construct($control, $action);
+      parent::__construct($control, $action);
     }
+
     public function gebruikerrecht(){
-        $typegebruiker = $this->model->getGebruikerRecht();
-        $this->view->set('typegebruiker', $typegebruiker);
+      $typegebruiker = $this->model->getGebruikerRecht();
+      $this->view->set('typegebruiker', $typegebruiker);
     }
 
-// kan de defaultAction niet in de AbstractController gezet worden?
     public function defaultAction() {
-        $this->gebruikerrecht();
-
-        $gebruikersnaam= $this->model->getGebruiker();
-        $this->view->set('gebruikersnaam',$gebruikersnaam);
+      $this->gebruikerrecht();
+      $gebruikersnaam= $this->model->getGebruiker();
+      $this->view->set('gebruikersnaam',$gebruikersnaam);
     }
 
     public function inschrijvenAction() {
-        $this->gebruikerrecht();
-        $gebruikersnaam= $this->model->getGebruiker();
-        $this->view->set('gebruikersnaam',$gebruikersnaam);
+      $this->gebruikerrecht();
+      $gebruikersnaam= $this->model->getGebruiker();
+      $this->view->set('gebruikersnaam',$gebruikersnaam);
 
-        $lesnamen= $this->model->getLesnaam();
-        $this->view->set('lesnamen',$lesnamen);
+      $inschrijvingen= $this->model->getInschrijvingen();
+      $this->view->set('inschrijvingen',$inschrijvingen);
+    }
 
-        $dates= $this->model->getLesdatum();
-        $this->view->set('dates',$dates);
-
-        $times= $this->model->getLestijd();
-        $this->view->set('times',$times);
-
-        if($this->model->isPostLeeg()){
-            echo 'vul de gegevens in';
-        }
-        else {
-            $this->model->addles();
-        }
+    public function inschrijvenlesAction(){
+      $this->model->addles();
+      $this->forward('inschrijven', 'lid');
     }
 
     public function lidBeheerAction() {
-        $this->gebruikerrecht();
-        $gebruikersnaam= $this->model->getGebruiker();
-        $this->view->set('gebruikersnaam',$gebruikersnaam);
+      $this->gebruikerrecht();
+      $gebruikersnaam= $this->model->getGebruiker();
+      $this->view->set('gebruikersnaam',$gebruikersnaam);
 
-        if($this->model->isPostLeeg()){
-            $this->view->set('note', 'vul de gegevens in');
+      if($this->model->isPostLeeg()){
+        $this->view->set('note', 'vul de gegevens in');
+      }
+      else{
+        $result = $this->model->updateGebruiker();
+        switch($result){
+          case REQUEST_SUCCESS:
+              $this->forward('default');
+              $this->view->set('note', 'Succes');
+              break;
+          case REQUEST_FAILURE_DATA_INCOMPLETE:
+              $this->view->set('note', 'De gegevens waren incompleet. Vul compleet in!');
+              break;
+          case REQUEST_NOTHING_CHANGED:
+              $this->view->set('note', 'Er was niets te wijzigen');
+              break;
+          case REQUEST_FAILURE_DATA_INVALID:
+              $this->view->set('note', 'Vul een correcte datum/tijd in.');
+              break;
         }
-        else{
-            $result = $this->model->updateGebruiker();
-            switch($result){
-                case REQUEST_SUCCESS:
-                    $this->forward('default');
-                    $this->view->set('note', 'Succes');
-                    break;
-                case REQUEST_FAILURE_DATA_INCOMPLETE:
-                    $this->view->set('note', 'De gegevens waren incompleet. Vul compleet in!');
-                    break;
-                case REQUEST_NOTHING_CHANGED:
-                    $this->view->set('note', 'Er was niets te wijzigen');
-                    break;
-                case REQUEST_FAILURE_DATA_INVALID:
-                    $this->view->set('note', 'Vul een correcte datum/tijd in.');
-                    break;
-            }
-        }
-        $gebruiker= $this->model->getGebruikerById();
-        $this->view->set('gebruiker',$gebruiker);
+      }
+      $gebruiker= $this->model->getGebruikerById();
+      $this->view->set('gebruiker',$gebruiker);
     }
 
     public function overzichtAction(){
-        $this->gebruikerrecht();;
-        $gebruikersnaam= $this->model->getGebruiker();
-        $this->view->set('gebruikersnaam',$gebruikersnaam);
-        $lessen = $this->model->lessonOverzicht();
-        $this->view->set('lessen',$lessen);
-        $islesaangemeld = $this->model->islesaangemeld();
-        $this->view->set('islesaangemeld',$islesaangemeld);
-        $overzichten=$this->model->lessonOverzicht();
-        $this->view->set('overzichten',$overzichten);
-
+      $this->gebruikerrecht();;
+      $gebruikersnaam= $this->model->getGebruiker();
+      $this->view->set('gebruikersnaam',$gebruikersnaam);
+      $lessen = $this->model->lessonOverzicht();
+      $this->view->set('lessen',$lessen);
+      $islesaangemeld = $this->model->islesaangemeld();
+      $this->view->set('islesaangemeld',$islesaangemeld);
+      $overzichten=$this->model->lessonOverzicht();
+      $this->view->set('overzichten',$overzichten);
     }
 
     public function  deelnemenAction(){
-        $this->model->deelnemerAanmelden();
-        $this->forward('overzicht');
-        $gebruikersnaam= $this->model->getGebruiker();
-        $this->view->set('gebruikersnaam',$gebruikersnaam);
+      $this->model->deelnemerAanmelden();
+      $this->forward('overzicht');
+      $gebruikersnaam= $this->model->getGebruiker();
+      $this->view->set('gebruikersnaam',$gebruikersnaam);
     }
 
     public function deletelesAction(){
-        $this->model->verwijderLes();
-        $this->forward('overzichtInschrijving');
+      $this->model->verwijderLes();
+      $this->forward('overzichtInschrijving');
     }
 }
--- app/codeproduction/view/tpls/lid/overzicht.php
+++ app/codeproduction/view/tpls/lid/overzicht.php
@@ -11,30 +11,21 @@
       </tr>
     </thead>
     <tbody>
-      <!-- <?php foreach ($overzichten as $overzicht){?>
-        <tr>
-          <td><?= $overzicht->getDate();?></td>
-          <td><?php echo $overzicht->getTime();?></td>
-          <td><?php echo $overzicht->getDescription();?></td>
-          <td><?php echo $overzicht->getAanmeldingen();?></td>
-          <td><a href='?control=lid&action=deleteles&id=<?php echo $overzicht->getId();?>'>Verwijder</a></td>
-        </tr> -->
-          <?php foreach ($lessen as $les){?>
-            <tr>
-                <td><?php echo $les->getDate();?></td>
-                <td><?php echo $les->getTime();?></td>
-                <td><?php echo $les->getlocation();?></td>
-                <td><?php echo $les->getDescription();?></td>
-                <?php if($les->getMax_persons()>$les->getAantalaangemeld()&&$islesaangemeld->getLesson_id()!==$les->getId()){
-                  echo "<td><a href='?control=lid&action=deelnemen&id=$les->getId()'>deelnemen</a></td>";
-                }?>
+        <?php foreach ($lessen as $les){?>
+          <tr>
+            <td><?php echo $les->getDate();?></td>
+            <td><?php echo $les->getTime();?></td>
+            <td><?php echo $les->getlocation();?></td>
+            <td><?php echo $les->getDescription();?></td>
+            <?php if($les->getMax_persons()>$les->getAantalaangemeld()&&$islesaangemeld->getLesson_id()!==$les->getId()){
+              echo "<td><a href='?control=lid&action=deelnemen&id=$les->getId()'>deelnemen</a></td>";
+            }?>
 
-                <?php if($les->getMax_persons()<=$les->getAantalaangemeld()){
-                  echo "<td> VOL! </td>";
-                }?>
-            </tr>
-          <?php }?>
-        <!-- <?php }?> -->
+            <?php if($les->getMax_persons()<=$les->getAantalaangemeld()){
+              echo "<td> VOL! </td>";
+            }?>
+          </tr>
+        <?php }?>
       </tbody>
     </table>
 </div>
